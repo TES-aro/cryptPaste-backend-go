03f600529bcbf5fe2d69338a17c9132c411e6c92
TES <taaviaro@student.uef.fi>
2026-01-31 15:21:17 +0200

    more test, introduced mux

--- connection/http_test.go
+++ connection/http_test.go
@@ -1,9 +1,56 @@
-package connection_test
+package connection
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-// Testing basic operations
+type handlerTest struct {
+	request *http.Request
+	wantCode int
+}
 
-func TestRequests(t *testing.T){
-	server :=
+type handlerTestArray struct {
+	testArray []handlerTest
+}
+
+func (h *handlerTestArray) newTest(method, url string, want int){
+	request , err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return
+	}
+	test := handlerTest{request, want }
+	h.testArray = append(h.testArray, test)
+}
+
+func TestHandlerFunctions(t *testing.T) {
+	tests := handlerTestArray{}
+	tests.newTest("GET", "/api/12345678", 200)
+	tests.newTest("GET","/api/1234", 404)
+	tests.newTest("POST", "/api/12345678", 405)
+
+	for _, tt := range tests.testArray {
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handleGET)
+		handler.ServeHTTP(rr, tt.request)
+		if rr.Code != tt.wantCode {
+			t.Errorf("Wanted status %d; got %d",tt.wantCode, rr.Code)
+			continue
+		}
+		fmt.Println(rr.Code , rr.Body)
+	}
+}
+
+func TestMux(t *testing.T) {
+	tests := handlerTestArray{}
+	testMux := createMux()
+
+	for _, tt := range tests.testArray {
+		rr := httptest.NewRecorder()
+		testMux.ServeHTTP(rr, tt.request)
+		if rr.Code != tt.wantCode {
+			t.Errorf("Wanted status ‰d; got %d", tt.wantCode, rr.Code)
+		}
 }
